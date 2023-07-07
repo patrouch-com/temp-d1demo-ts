e10f675686958c0d49ec8176e1447fd1f4e1dc9a
Chef <cheftech@patraldo.dev>
2023-07-06 19:39:00 -0600

    change skeleton h1 text and add style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Bienvenid@s a Mexican Bold</h1>
+
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Marmelad&family=Pacifico&family=Special+Elite&family=Tangerine:wght@400;700&family=Ultra&display=swap');
+h1 {
+font-family: 'Special Elite', cursive;
+font-size: 3.0rem;
+padding: 20px 20px 60px;
+max-width: 680px;
+margin: 0 auto;
+}
+
+</style>
